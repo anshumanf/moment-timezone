15f115c973cf199fd8f4d24b8f38009017968431
Fotedar, Anshuman (WorldQuant) <Anshuman.Fotedar@worldquant.com>
2017-05-09 14:43:53 -0400

    Make scoped package and depend on scoped 'moment'

--- moment-timezone-utils.js
+++ moment-timezone-utils.js
@@ -9,7 +9,7 @@
 
 	/*global define*/
 	if (typeof define === 'function' && define.amd) {
-		define(['moment'], factory);                 // AMD
+		define(['@anshumanf/moment'], factory);      // AMD
 	} else if (typeof module === 'object' && module.exports) {
 		module.exports = factory(require('./'));     // Node
 	} else {
--- moment-timezone.js
+++ moment-timezone.js
@@ -9,11 +9,11 @@
 
 	/*global define*/
 	if (typeof define === 'function' && define.amd) {
-		define(['moment'], factory);                 // AMD
+		define(['@anshumanf/moment'], factory);                 // AMD
 	} else if (typeof module === 'object' && module.exports) {
-		module.exports = factory(require('moment')); // Node
+		module.exports = factory(require('@anshumanf/moment')); // Node
 	} else {
-		factory(root.moment);                        // Browser
+		factory(root.moment);                                   // Browser
 	}
 }(this, function (moment) {
 	"use strict";
